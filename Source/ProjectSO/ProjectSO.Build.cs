7cad3c0cf8e1f35b3c52b086d96b1118f313c338
EricJang <ericjangdev@gmail.com>
2024-07-14 15:41:41 +0900

    [장명훈][SO-2] 프로젝트 생성
    ProjectSO 업로드

---
+++ Source/ProjectSO/ProjectSO.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ProjectSO : ModuleRules
+{
+	public ProjectSO(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
