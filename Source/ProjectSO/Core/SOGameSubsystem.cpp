b6be8c5250fc38f8758de6cdf09d8e915a5f495b
YUN SOO KIM <118554050+Kim-yunsoo@users.noreply.github.com>
2024-08-09 19:53:47 +0900

    [김윤수][SO-72] Damage Data Table 코드 작성
    Bone , Gun 종류에 따라 다른 데미지가 들어가도록 Data Setting

--- Source/ProjectSO/Core/SOGameSubsystem.cpp
+++ Source/ProjectSO/Core/SOGameSubsystem.cpp
@@ -22,7 +22,8 @@ USOGameSubsystem::USOGameSubsystem()
 	}
 
 	ConstructorHelpers::FObjectFinder<UDataTable> SpanwableDataRef(
-		TEXT("/Script/Engine.DataTable'/Game/StellarObsidian/GameData/DT_SOSpanwnableItemClasses.DT_SOSpanwnableItemClasses'"));
+		TEXT(
+			"/Script/Engine.DataTable'/Game/StellarObsidian/GameData/DT_SOSpanwnableItemClasses.DT_SOSpanwnableItemClasses'"));
 	if (SpanwableDataRef.Succeeded())
 	{
 		SpawnableItemDataTable = SpanwableDataRef.Object;
@@ -31,17 +32,26 @@ USOGameSubsystem::USOGameSubsystem()
 	}
 
 	ConstructorHelpers::FObjectFinder<UDataTable> WeaponDamageDataRef(
-	TEXT("/Script/Engine.DataTable'/Game/StellarObsidian/GameData/DT_SOWeaponDamageData.DT_SOWeaponDamageData'"));
+		TEXT("/Script/Engine.DataTable'/Game/StellarObsidian/GameData/DT_SOWeaponDamageData.DT_SOWeaponDamageData'"));
 	if (WeaponDamageDataRef.Succeeded())
 	{
 		WeaponDamageTable = WeaponDamageDataRef.Object;
+	}
+
+	ConstructorHelpers::FObjectFinder<UDataTable> WeaponDamageByBoneRef(
+		TEXT("/Script/Engine.DataTable'/Game/StellarObsidian/GameData/DT_SOWeaponDamageByBone.DT_SOWeaponDamageByBone'"));
+	if (WeaponDamageDataRef.Succeeded())
+	{
+		WeaponDamageByBoneTable = WeaponDamageByBoneRef.Object;
 	}
 }
 
 void USOGameSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
 	Super::Initialize(Collection);
+	UE_LOG(LogTemp, Warning, TEXT("Initialize "));
 
+	ProcessWeaponDamageDataRows();
 }
 
 USOGameSubsystem* USOGameSubsystem::GetSOGameSubsystem()
@@ -110,7 +120,7 @@ FSOWeaponDamageData* USOGameSubsystem::GetWeaponDamageData(const uint8 InID)
 
 	FString RowName = FString::Printf(TEXT("%d"), InID);
 
-	FSOWeaponDamageData* WeaponDamageDataRow = WeaponDataTable->FindRow<FSOWeaponDamageData>(FName(*RowName), "");
+	FSOWeaponDamageData* WeaponDamageDataRow = WeaponDamageTable->FindRow<FSOWeaponDamageData>(FName(*RowName), "");
 	if (WeaponDamageDataRow)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Found WeaponDamageTable for ID: %d"), InID);
@@ -143,4 +153,61 @@ FSOSpawnableItemClasses* USOGameSubsystem::GetSpawnableItemData(const int32 InIn
 		UE_LOG(LogTemp, Warning, TEXT("No WeaponDataTable found for ID: %d"), InIndex);
 	}
 	return SpawnableDataRow;
+}
+
+//Initialize 에서 ProcessWeaponDamageDataRows 호출
+//이 함수는 Weapon Damage by bone 에 대한 정보를 가져온다. (파싱? )
+void USOGameSubsystem::ProcessWeaponDamageDataRows() 
+{
+	if (!WeaponDamageTable)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No WeaponDamageTable found "));
+
+		return;
+	}
+	TArray<FName> RowNames = WeaponDamageTable->GetRowNames();
+	int32 NumDamageDataRows = RowNames.Num();
+
+	for (int32 ID = 0; ID < NumDamageDataRows; ++ID)
+	{
+		FSOWeaponDamageData* DamageDataTable = GetWeaponDamageData(ID); //행 가져오기 
+
+		if (DamageDataTable)
+		{
+			for (TFieldIterator<FProperty> It(FSOWeaponDamageData::StaticStruct()); It; ++It)
+			{
+				FProperty* Property = *It;
+				FString PropertyName = Property->GetName();
+				FString PropertyValue;
+				// Property Value 값 넣어주기 
+				Property->ExportText_InContainer(0, PropertyValue, DamageDataTable, DamageDataTable, nullptr, PPF_None);
+				InitializeWeaponDamageByBoneTable( ID, PropertyName ,PropertyValue);
+			}
+		}
+	}
+}
+
+//WeaponDamageByBoneTable에 실질적인 데이터 Setting 진행 
+void USOGameSubsystem::InitializeWeaponDamageByBoneTable(const int32 InID , const FString& InBoneName, const FString& InDamageValue)
+{
+	if(!WeaponDamageByBoneTable)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No WeaponDamageByBoneTable found "));
+		return ; 
+	}
+	
+	FString RowName = FString::Printf(TEXT("%d"), InID);
+
+	FSOWeaponDamageByBone* WeaponDamageByBoneDataRow = WeaponDamageByBoneTable->FindRow<FSOWeaponDamageByBone>(
+		FName(*RowName), "");
+	if (WeaponDamageByBoneDataRow)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Found WeaponDamageByBoneTable for ID: %d"), InID);
+		float InDamageFloatValue = FCString::Atof(*InDamageValue);
+		WeaponDamageByBoneDataRow->DamageByHitLocationMap.Add(InBoneName,InDamageFloatValue);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No WeaponDamageByBoneTable found for ID: %d"), InID);
+	}
 }
